5cac36e680a7a9dc423687a32d5967ad5d2ef6a7
Eduardo Stuani <eduardostuani.cs@gmail.com>
2022-09-03 21:04:22 -0300

    solved 015, 016

---
+++ 015_lattice_paths.py
@@ -0,0 +1,25 @@
+#
+# Eduardo Stuani Solution(s) for Project Euler Problem Number
+# https://github.com/stuaninauts
+#
+# Starting in the top left corner of a 2×2 grid, and only being able
+# to move to the right and down, there are exactly 6 routes to the bottom right corner.
+#
+#
+# How many such routes are there through a 20×20 grid?
+import math
+
+
+# Solution 1
+#
+# For this problem we can use the combination formula -> C = n! / (n-p)! * p!
+# C = routes
+# n = the number of moves = grid size * 2
+# p = the size of the grid = size
+def solve():
+    size = 20
+    return math.factorial(size*2) / (math.factorial(size) * math.factorial(size))
+
+
+if __name__ == "__main__":
+    print(solve())
---
+++ 016_power_digit_sum.py
@@ -0,0 +1,19 @@
+#
+# Eduardo Stuani Solution(s) for Project Euler Problem Number
+# https://github.com/stuaninauts
+#
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+#
+# What is the sum of the digits of the number 2^1000?
+
+
+# Solution 1
+def solve():
+    n = 2
+    e = 1000
+    x = n**e
+    return sum(int(d) for d in str(x))
+
+
+if __name__ == "__main__":
+    print(solve())
